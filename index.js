d8a5e85e9f4e0227a32a1118495489ba6341e264
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-03 13:50:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Performing a Task
+function greet(name, lastName)
+{
+    console.log("Hello " + name + " " + lastName + "!");
+}
+
+//Calculating a Value
+function square(number)
+{
+    return number*number; 
+}
+
+greet("Farhan", "Hasan"); 
+
+let squaredNumber = square(5); 
+console.log(squaredNumber);
+console.log(square(6)); //Another way to display the function output 
